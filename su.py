005a4d58c9b9c107192fd0708148413b625eb1b5
nvetrov@gmail.com <nvetrov@gmail.com>
2018-06-02 13:37:51 +0300

    save new

---
+++ su.py
@@ -0,0 +1,19 @@
+print('hi')
+
+# def suger():
+#     s = []
+#     for i in 'ser':
+#         s.append(i)
+#     print(s)
+#
+# #suger()
+#
+# print('--------------------')
+# s = [i for i in 'ser']
+# #print(s)
+#
+# ss = [ w for w in range(1,14) if w % 2 == 0]
+# ss1 = [ w for w in range(1,14) if w % 2 != 0]
+# print(ss)
+# print(ss1)
+#
