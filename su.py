a697713226274de86fd25cea27bc716dfafff8bb
NIKOLAY VETROV <nvetrov@gmail.com>
2018-06-12 12:35:35 +0300

    IO python

--- su.py
+++ su.py
@@ -1,19 +1,28 @@
-print('hi')
+def write_to_file(filename, content, mode='w'):
+    with open(filename, mode=mode) as f:
+        f.write(content)
 
-# def suger():
-#     s = []
-#     for i in 'ser':
-#         s.append(i)
-#     print(s)
-#
-# #suger()
-#
-# print('--------------------')
-# s = [i for i in 'ser']
-# #print(s)
-#
-# ss = [ w for w in range(1,14) if w % 2 == 0]
-# ss1 = [ w for w in range(1,14) if w % 2 != 0]
-# print(ss)
-# print(ss1)
-#
+
+
+def way_batter(filename):
+    print('Читаем файл  way_batter')
+    with open(filename) as f:
+        return f.read()
+
+
+def read_file(filename):
+    print('Читаем файл read_file')
+    try:
+        f = open(filename)
+        context = f.read()
+    finally:
+        # f.close()
+        print('Не удалось открыть файл методом read_file')
+
+
+
+if __name__ == '__main__':
+    # reading
+    # print(way_batter('HomeWorks.txt'))
+    print(read_file('HomeWorks.txt'))
+    write_to_file(filename='NewFile.txt', content='NewLine\n', mode='a') #adds
