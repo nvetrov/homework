183896fcd1b26fa1ac368cab19b936bcbb026335
nvetrov@gmail.com <nvetrov@gmail.com>
2018-06-16 15:47:05 +0300

    json

--- web.py
+++ web.py
@@ -1,20 +1,46 @@
-from requests import get
+# from requests import get
+import requests
 
+__author__ = 'vetrov nikolay'
+'''
 with open('dataset_3378_2 (1).txt', 'r') as inf:
-    x = get(inf.readline().strip())
+    x = requests.get(inf.readline().strip())
     print((x.text))
 
-# import requests
-# url = 'https://api.github.com/user'
-#
-# r = requests.get(url, auth=('nvetrov@gmail.com', 'C1vmdpalc33'))
-# cookies = {'cookies_are':'working'}
-# print(r.status_code) # 200 все хорошо.
-# print(r.headers['content-type'])
-# print(r.encoding)
-# print(r.text)
-# print('--------')
-# print(r.content)
-# print('--------')
-# r = requests.get(url,cookies=cookies)
-# print(r.cookies)
+url = 'https://api.github.com/user'
+
+r = requests.get(url, auth=('nvetrov@gmail.com', 'C1vmdpalc33'))
+cookies = {'cookies_are':'working'}
+print(r.status_code) # 200 все хорошо.
+print(r.headers['content-type'])
+print(r.encoding)
+print(r.text)
+print('--------')
+print(r.content)
+print('--------')
+r = requests.get(url,cookies=cookies)
+print(r.cookies)
+'''
+
+
+def get_url():
+    r = requests.get('http://petstore.swagger.io/v2/pet/1')
+    print('r.status_code->', r.status_code)  # 200 все хорошо.
+    print("r.headers['content-type']->", r.headers['content-type'])
+    print('r.encoding->', r.encoding)
+    print("r.text ->", r.text)
+
+
+def findByStatus(tag):
+    params = {'tags': tag}
+    headers = {'Accept': 'application/json'}
+    # {'Accept':'application/xml'}
+    url = 'http://petstore.swagger.io/v2/pet/findByStatus?status=available'
+    r = requests.get(url, params=params, header=headers)
+    print(r.status_code, r.headers)
+    print(r.content)
+
+
+if __name__ == '__main__':
+    # get_url()
+    findByStatus()
